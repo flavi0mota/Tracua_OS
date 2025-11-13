b860959aabeaf38e85f98c9a6fa275b55e924d2f
Flávio Mota <mendelmann1988@gmail.com>
2025-11-13 17:46:20 -0300

    Newline implemented

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -40,11 +40,20 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
+	unsigned char newline = '\n';
+	
+	if (uc == newline) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
+		}
+	} else {
+		terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+				terminal_row = 0;
+		}
 	}
 }
 
